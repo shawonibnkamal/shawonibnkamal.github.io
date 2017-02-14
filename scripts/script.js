01b6d3c870157a0916c8adb76b69b7ba33ad9bfe
Shawon Ibn Kamal <shawon.1165@gmail.com>
2017-02-14 22:54:37 +0600

    new projects, few fixes

--- scripts/script.js
+++ scripts/script.js
@@ -18,18 +18,17 @@ $(document).ready(function() {
 	
 	$(function() {
 	  $('.nav-link').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 		  var target = $(this.hash);
 		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 		  if (target.length) {
 			$('html, body').animate({
-			  scrollTop: target.offset().top
+			  scrollTop: target.offset().top - 50
 			}, 600);
 			return false;
 		  }
-		}
 	  });
 	});
+	
 	
 	
 });
